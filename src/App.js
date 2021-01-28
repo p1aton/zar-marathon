94ec70f96df8d861773a2512233d7dc4ac826837
p1aton <ovk.job@gmail.com>
2021-01-28 19:00:03 +0300

    correction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Header from './components/Header/Header';
-import Layout from './components/Layout/Layout';
-import Footer from './components/Footer/Footer';
+import Header from './components/Header';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
 import bgImg1 from './assets/bg1.jpg';
 import bgImg2 from './assets/bg2.jpg';
 
 const App = () => {
   return (
     <>
-      <Header title='This is a header title' descr='and description' />
+      <Header title={'This is title'} descr={'This is Description!'} />
       <Layout id={1}  title='Layout 1 title' descr='description' urlBg={bgImg1} />
       <Layout id={2}  title='Layout 2 title' descr='description' colorBg='#777' />
       <Layout id={3}  title='Layout 3 title' descr='description' urlBg={bgImg2} />
